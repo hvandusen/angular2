6d44298e8e6dd33ec878410e96502723fb19ed80
henry van dusen <hgv2102@columbia.edu>
2015-04-14 00:16:49 -0400

    massive commit for whole project

---
+++ public/js/controllers/MainController.js
@@ -0,0 +1,37 @@
+app.controller('MainController', function ($scope, FlashCardsFactory, ScoreFactory) {
+	FlashCardsFactory.getFlashCards().then(function(yah){
+		$scope.flashCards = yah
+	});
+	$scope.answerQuestion = function (answer, flashCard) {
+		if (!flashCard.answered) {
+			flashCard.answered = true;
+			flashCard.answeredCorrectly = answer.correct;
+			if(answer.correct)
+			ScoreFactory.correct++;
+			else
+			ScoreFactory.incorrect;
+		}
+	}
+	$scope.categories = [
+    'MongoDB',
+    'Express',
+    'Angular',
+    'Node',
+		'All'
+];
+	$scope.cat = 'All'
+	$scope.getCategoryCards = function(category){
+		$scope.cat = category
+		$scope.flashCards = FlashCardsFactory.getFlashCards(category).then(function(yah){
+			$scope.flashCards = yah
+		});
+	};
+});
+
+app.filter('cheat',function(){
+	return function(collection){
+		return collection.filter(function(element){
+			return element.correct
+		});
+	}
+})
